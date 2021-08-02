6db998792de273a61c39848ff0f782680aadc8d4
pubudu <pubudusachintha1996@gmail.com>
2021-08-02 20:06:17 +0530

    added bootrap ui for auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Auth group
+Route::group(['middleware' => ['auth']], function () {
+
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
