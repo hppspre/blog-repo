e9b2096e83d3a414e3ca872bcc0d9db32f4e4a48
pubudu <pubudusachintha1996@gmail.com>
2021-08-03 15:44:32 +0530

    finalized

--- database/migrations/2021_08_03_055512_create_comments_table.php
+++ database/migrations/2021_08_03_055512_create_comments_table.php
@@ -16,14 +16,9 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
 
             $table->increments("id");
-
-            $table->foreignId('post_id')->unsigned()->index()->nullable();
-            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
-
+            $table->integer('post_id');
             $table->foreignId('user_id')->unsigned()->index()->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-
-
             $table->longText('comment');
             $table->timestamps();
         });
